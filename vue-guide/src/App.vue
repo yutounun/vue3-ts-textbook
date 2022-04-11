166e18485236de4f731690e3b6c1fc5a906e432a
Yuto Ichihara <yutounwasese@gmail.com>
2022-04-11 19:13:15 +0900

    4/11

--- vue-guide/src/App.vue
+++ vue-guide/src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-</script>
+import TodoList from './components/TodoList.vue';
+import { provide } from 'vue';
+// これはemitなど不必要。子コンポーネントから来ているのではなく、ただの別ファイルから来ているため
+import { todos, todoKey } from './usetodo'
+// exportされたtodosをvalueに、'todos'をkeyに
+// provideしたcomponentの子componentsはinjectできる → 一番親でprovideした方がいい
+// provide('todos', todos)
+// injectionKeyを使うことで変数かつ一意にできる。型を使える
+provide(todoKey, todos)
 
+</script>
 <template>
   <div>
-    
+    <TodoList />
   </div>
 </template>
 
--- vue-guide/src/components/TodoList.vue
+++ vue-guide/src/components/TodoList.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-const todos = []
+import { inject } from 'vue';
+import { todoKey } from '../usetodo'
+
+const { todos, addTodo: _addTodo } = inject(todoKey)
+// const _addTodo = inject('addTodo')
 
 const addTodo = (title: string) => {
-  console.log(title)
+  _addTodo(title)
 }
 
 </script>
@@ -10,10 +14,7 @@ const addTodo = (title: string) => {
 <template>
   <div>
     <ul :style="{ listStyle: 'none' }">
-      <li
-        :style="{ backgroundColor: 'aliceblue', marginBottom: '10px' }"
-        v-for="todo in todos"
-      >{{ todo.title }}</li>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
     </ul>
     <button @click="addTodo('add!')">Add</button>
   </div>
